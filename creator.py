5d063efa6bf2e1ef1896a5dc5e4e21bc7782ddcc
AskarKasimov <kasimovaskar123@gmail.com>
2021-11-01 22:05:31 +0400

    начало

--- creator.py
+++ creator.py
@@ -1,11 +1,12 @@
 import sys
-import sqlite3
+
 # Импортируем из PyQt5.QtWidgets классы для создания приложения и виджета
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton
+from PyQt5.Qt import QFont
 
 
 # Унаследуем наш класс от простейшего графического примитива QWidget
-class Note(QWidget):
+class Example(QWidget):
     def __init__(self):
         # Надо не забыть вызвать инициализатор базового класса
         super().__init__()
@@ -15,9 +16,22 @@ class Note(QWidget):
 
     def initUI(self):
         # Зададим размер и положение нашего виджета,
-        self.setGeometry(300, 300, 300, 300)
+        self.setGeometry(300, 300, 700, 300)
         # А также его заголовок
-        self.setWindowTitle('Заметка')
+        self.setWindowTitle('Создать заметку')
+
+        self.label = QLabel(self)
+        self.label.setFont(QFont('Arial', 30))
+        self.label.move(int(self.frameGeometry().getRect()[2] / 2.5), int(self.frameGeometry().getRect()[3] / 4))
+        self.label.setText("SmartNotes")
+
+        self.btn_create = QPushButton(self)
+        self.btn_create.setText("Создать заметку")
+        self.btn_create.move(int(self.frameGeometry().getRect()[2] / 2.5), int(self.frameGeometry().getRect()[3] / 2))
+
+        self.btn_remove = QPushButton(self)
+        self.btn_remove.setText("Удалить заметку")
+        self.btn_remove.move(int(self.frameGeometry().getRect()[2] / 1.8), int(self.frameGeometry().getRect()[3] / 2))
 
 
 if __name__ == '__main__':
@@ -25,8 +39,8 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     # А теперь создадим и покажем пользователю экземпляр
     # нашего виджета класса Example
-    note = Note()
-    note.show()
+    ex = Example()
+    ex.show()
     # Будем ждать, пока пользователь не завершил исполнение QApplication,
     # а потом завершим и нашу программу
     sys.exit(app.exec())
---
+++ note.py
@@ -0,0 +1,51 @@
+import sys
+import sqlite3
+from win32api import GetSystemMetrics
+import random
+# Импортируем из PyQt5.QtWidgets классы для создания приложения и виджета
+from PyQt5.QtWidgets import QApplication, QWidget
+
+
+# Унаследуем наш класс от простейшего графического примитива QWidget
+class Note(QWidget):
+    def __init__(self, title, text, pic, author):
+        self.title = title
+        self.text = text
+        self.pic = pic
+        self.author = author
+        # Надо не забыть вызвать инициализатор базового класса
+        super().__init__()
+        # В метод initUI() будем выносить всю настройку интерфейса,
+        # чтобы не перегружать инициализатор
+        self.initUI()
+
+    def initUI(self):
+        # Зададим размер и положение нашего виджета,
+        self.setGeometry(random.randint(0, GetSystemMetrics(0) - 300), random.randint(0, GetSystemMetrics(1) - 300),
+                         300, 300)
+        # А также его заголовок
+        self.setWindowTitle(self.title)
+
+
+if __name__ == '__main__':
+    # Список со всеми заметками
+    notes = list()
+    # Создадим класс приложения PyQT
+    app = QApplication(sys.argv)
+    # Подключение к БД
+    con = sqlite3.connect("notes.db")
+    # Создание курсора
+    cur = con.cursor()
+    # Выполнение запроса и получение всех результатов
+    result = cur.execute("""SELECT note_title FROM notes""").fetchall()
+    # Запись заметок в список
+    for elem in result:
+        notes.append(Note(elem[0], None, None, None))
+    # Закрытие подключения
+    con.close()
+    # Вывод всех заметок на экран
+    for note in notes:
+        note.show()
+    # Будем ждать, пока пользователь не завершил исполнение QApplication,
+    # а потом завершим и нашу программу
+    sys.exit(app.exec())
